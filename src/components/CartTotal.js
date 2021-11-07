0f058ce2baeaecb02043f07edf30508a69f90b26
Mandor99 <mahmoudmandor65@gmail.com>
2021-11-07 14:21:29 +0200

    fix conflict1

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -2,7 +2,26 @@ import React from 'react'
 import CurrencyFormat from 'react-currency-format';
 import { MainBtn } from '../styles/GlobalStyle';
 import { Total } from '../styles/ShoppingCartStyle';
+import {useCart} from '../features/cartContext'
+import {useHistory} from 'react-router-dom'
+import {getTotal} from '../features/cartReducer'
 
+function CartTotal() {
+
+    const history = useHistory()
+    const {cart} = useCart()
+
+    const paymentRedirect = () => {
+        cart?.cart.length > 0 && history.push('/payment')
+    }
+
+    return (
+        <CurrencyFormat value={getTotal(cart?.cart)} displayType={'text'} thousandSeparator={true} prefix={'$'} allowNegative={false} fixedDecimalScale={true} decimalScale={2} thousandSpacing={'3'} renderText={(val) => <Total>
+            <h3 className='total__title'>Subtotal ({cart?.cart.length} items): <strong>{val}</strong></h3>
+            <div className='total__gift'>
+                <input type="checkbox" name="gift" id="gift" /><span>this order contains a gift</span>
+            </div>
+            <MainBtn onClick={paymentRedirect} className='total__btn'>proceed to checkout</MainBtn>
         </Total>}/>
             
     )
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,6 +2,12 @@ import React from 'react'
 import CartTotal from '../components/CartTotal';
 import Navbar from '../components/Navbar'
 import {Main, Items, Ad, ItemProducts} from '../styles/ShoppingCartStyle'
+import {useCart} from '../features/cartContext'
+import CartProducts from '../components/CartProducts';
+
+function ShoppingCart() {
+    const {cart} = useCart()
+    // const show = false;
 
     return (
         <>
@@ -12,7 +18,11 @@ import {Main, Items, Ad, ItemProducts} from '../styles/ShoppingCartStyle'
                         <img className="ad__img" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/AMZN_OutletDeals_Template_March_1500x200_wh_EN.jpg" alt="Ad"/>
                     </Ad>
                     <ItemProducts>
-
+                        {cart?.cart.length <= 0 ? (<h3 className="items__title">your amazon cart is empty</h3>) : (<CartProducts cart={cart?.cart} title={'Shopping Cart'} showDeleteBtn={true}/>)}
+                    </ItemProducts>
+                </Items>
+                {
+                    cart?.cart.length > 0 && (<CartTotal />)
                 }
             </Main>
         </>
