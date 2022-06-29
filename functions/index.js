bdf75f726c12baf44fa47f8468d45fceefe33328
Mandor99 <mahmoudmandor65@gmail.com>
2022-06-29 19:55:39 +0200

    edit some ui

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,22 @@
 const functions = require('firebase-functions');
 const express = require('express')
 require('dotenv').config()
-const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 const cors = require('cors')
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
+console.log(`${process.env.STRIPE_SECRET_KEY}`)
 
 //CONFIG
 const app = express()
 
 //MIDDLEWARE
-app.use(cors())
+app.use(cors({origin: true}))
 app.use(express.json())
 
 //ROUTS
-app.get('/', (req, res) => res.status(200).send('hello AMAZON!'))
+app.get('/', (req, res) => res.status(200).send(`hello AMAZON!`)) //in server endpoint
 app.post('/payments/create', async(req, res) => {
     const {total} = req.query
-    console.log(`payment received >>> ${total}`)
+    console.log(`payment received >>> ${total} for client secret >>> ${process.env.STRIPE_SECRET_KEY}`)
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount: total, // stripe calc it with cent so i multiple total * 100
